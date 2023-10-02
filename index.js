95424019bf21000ffbb64210ebb815f1bb47689d
william <willpatterson5802@gmail.com>
2023-10-02 15:38:57 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(feet){
+  if(400 >= feet) {
+    return "This one is on me!"
+  } else if (2000 > feet) {
+    return "That will be twenty bucks."
+  } else if (2500> feet && feet > 2000) {
+    return "I will gladly take your thirty bucks."
+  } else if (feet > 2500) {
+    return "No can do."
+  }
+    
+  }
+  
+
+function ternaryCheckCity(city) {
+  if(city === "NYC") {
+    return "Ok, sounds good."
+  } else if (city === "Pittsburgh") {
+    return "No go."
+  }
+  
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  if(tip === "generous") {
+    return "Thank you so much."
+  } else if (tip === "not as generous") {
+    return "Thank you."
+  } else if (tip === "thanks for everything") {
+    return "Bye."
+  }
 }
